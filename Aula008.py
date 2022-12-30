fc6a8ffbdb05228de41cca2430fbf9a3b1cfffba
Lucas Alves <Ei.lucas.alves@outlook.com>
2022-12-30 18:19:53 -0300

    Aula008

---
+++ Aula008.py
@@ -0,0 +1,12 @@
+nome = 'Lucas'
+sobrenome = 'Alves'
+idade = 26
+anoNascimento = 1996
+maiorDeIdade = idade >= 18
+alturaMetros = 1.90
+
+print('Nome:',nome,sobrenome)
+print('Idade:',idade)
+print('Ano de nascimento:',anoNascimento)
+print('É maior de idade?', maiorDeIdade)
+print('Altura em metros', alturaMetros)
