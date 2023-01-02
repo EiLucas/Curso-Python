5bd0370bc6fc40d865f4642cc9f11d0270c80d67
Lucas Alves <Ei.lucas.alves@outlook.com>
2023-01-02 16:26:48 -0300

    Aula010

--- Aula010.py
+++ Aula010.py
@@ -1,2 +1,11 @@
 concatenacao = 'Lucas' + ' ' + 'Alves'
 print(concatenacao)
+
+aDezVezes = 'A' * 100
+print(aDezVezes)
+
+tresVezesLucas = 3 * 'Lucas'
+print(tresVezesLucas)
+
+concatenacao2 = 'Lucas' + ' ' + str(5) + ' ' + 'Alves'
+print(concatenacao2)
