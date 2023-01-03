a18767c70bc9f209f1ee40b2602d1b13a5f86d74
Lucas Alves <Ei.lucas.alves@outlook.com>
2023-01-03 10:53:13 -0300

    Aula015

---
+++ Aula015.py
@@ -0,0 +1,13 @@
+#nome = input('Qual o seu nome? ')
+#print(f'Bem vindo {nome}')
+
+#numero_1 = int(input('Digite um numero: '))
+#numero_2 = int(input('Digite outro numero: '))
+
+numero_1 = input('Digite um numero: ')
+numero_2 = input('Digite outro numero: ')
+
+int_numero_1 = int(numero_1)
+int_numero_2 = int(numero_2)
+
+print(f'A soma dos numeros é: {int_numero_1 + int_numero_2}')
