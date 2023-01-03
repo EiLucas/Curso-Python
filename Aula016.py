7aa857774e97e8b83864ff2879d6e115a4cdd4e4
Lucas Alves <Ei.lucas.alves@outlook.com>
2023-01-03 11:06:43 -0300

    Aula016

---
+++ Aula016.py
@@ -0,0 +1,12 @@
+# if(se) / elif(se não se) / else(se não)
+
+entrada = input('você quer "entrar" ou "sair"? ')
+
+if entrada == 'entrar':
+    print('Você entrou no sistema.')
+
+elif entrada == 'sair':
+    print('Você saiu do sistema.')
+
+else:
+    print('Você não digitou "entrar" e nem "sair".')    
