325c80bfcdc2be07578ed35153a002dd3eb8f39f
Lucas Alves <Ei.lucas.alves@outlook.com>
2023-01-03 13:16:23 -0300

    Aula018

---
+++ Aula018.py
@@ -0,0 +1,17 @@
+# if(se) / elif(se não se) / else(se não)
+
+condicao1 = False
+condicao2 = False
+condicao3 = False
+condicao4 = False
+
+if condicao1:
+    print('Código para condição 1')
+elif condicao2:
+    print('Código para condição 2')
+elif condicao3:
+    print('Código para condição 3')
+elif condicao4:
+    print('Código para condição 4')
+else:
+    print('Nenhuma condição foi satisfeita.')
