ad7762299d8bbbaccf7b3882b43c234fe4dba6b7
Lucas Alves <Ei.lucas.alves@outlook.com>
2023-01-05 19:09:54 -0300

    Aula020

--- Aula020.py
+++ Aula020.py
@@ -2,8 +2,8 @@ valor_1 = input('Digite o primeiro valor: ')
 valor_2 = input('Digite o segundo valor: ')
 
 if valor_1 > valor_2:
-    print('o primeiro valor é maior que o segundo valor!')
+    print(f'o primeiro valor {valor_1} é maior que o segundo valor {valor_2}!')
 elif valor_2 > valor_1:
-    print('o segundo valor é maior que o primeiro valor!')
+    print(f'o segundo valor {valor_2} é maior que o primeiro valor {valor_1}!')
 else:
-    print('os valores são iguais!')        
+    print(f'os valores são iguais! {valor_1} e {valor_2}')        
