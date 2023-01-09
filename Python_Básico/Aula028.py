991c3c7ac22eca254a823560ac948cc5ce9118a7
Lucas Alves <Ei.lucas.alves@outlook.com>
2023-01-09 17:30:52 -0300

    Aula028
    Exercício

--- Python_Básico/Aula028.py
+++ Python_Básico/Aula028.py
@@ -13,3 +13,22 @@ Se nome e idade forem digitados:
 Se nada for digitado em nome ou idade:
     Exiba "Desculpe, você deixou campos vazios."
 """
+nome = input('Digite seu nome: ')
+idade = input('Digite sua idade: ')
+
+if nome and idade:
+    print(f'Seu nome é {nome}')
+    print(f'Seu nome infertido é {nome[::-1]}') 
+
+    if ' ' in nome:
+        print('Seu nome contém espaços')
+    else:    
+        print('Seu nome contém não espaços')
+
+    print(f'Seu nome tem {len(nome)} letras')
+#ou print('Seu nome tem', len(nome) ,'letras')
+
+    print(f'A primeira letra do seu nome é {nome[0]}')
+    print(f'A última letra do seu nome é {nome[4]}')
+else:
+    print('Desculpe, você deixou campos vazios.')    
