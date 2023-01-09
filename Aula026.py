e2924eff704330ae45651aef205ae359f26fe4fe
Lucas Alves <Ei.lucas.alves@outlook.com>
2023-01-08 21:02:04 -0300

    Aula026

---
+++ Aula026.py
@@ -0,0 +1,15 @@
+# Interpolação básica de strings
+# s - string
+# d e i - int
+# f - float
+# .<número de dígitos>f
+# x e X - Hexadecimal (ABCDEF123456789)
+# > - Esquerda 
+# < - Direita
+# ^ - Centro
+# Sinal - + ou -
+# Ex.: 0>-100,.1f
+# Conversion flags - !r !s !a
+
+variavel = 'ABC'
+print(f'{variavel}')
