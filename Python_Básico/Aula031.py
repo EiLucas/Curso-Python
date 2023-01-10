97ce83dd1e6fca1bfcb757e43c4dc45c4b2272cd
Lucas Alves <Ei.lucas.alves@outlook.com>
2023-01-10 15:07:00 -0300

    Aula031
    Flag (Bandeira) - Marca um local
    None = não valor
     is e is not = é ou não é (tipo, valor, identidade)
    id = identidade

---
+++ Python_Básico/Aula031.py
@@ -0,0 +1,11 @@
+# Flag (Bandeira) - Marca um local
+# None = não valor
+# is e is not = é ou não é (tipo, valor, identidade)
+# id = identidade
+
+v1 = 'a' 
+v2 = 'a' # mesmo endereço de memória, por terem o mesmo valor.
+
+print(id(v1)) 
+print(id(v2)) 
+
