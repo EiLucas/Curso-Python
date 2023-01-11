eefb81e505a8184ff8237018b5b339cfcfdba066
Lucas Alves <Ei.lucas.alves@outlook.com>
2023-01-11 20:36:59 -0300

    Aula031

--- Python_Básico/Aula031.py
+++ Python_Básico/Aula031.py
@@ -9,3 +9,19 @@ v2 = 'a' # mesmo endereço de memória, por terem o mesmo valor.
 print(id(v1)) 
 print(id(v2)) 
 
+condicao = True
+passou_no_if = None
+
+if condicao:
+    passou_no_if = True
+    print('Faça algo')
+else:
+    print('Não faça algo')
+
+if passou_no_if is None:
+    print('Não passou no if')
+else:
+    print('Passou no if')
+    
+#if passou_no_if is not None:
+#    print('Passou no if')    
