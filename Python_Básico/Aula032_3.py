0401d542947cb3050f4e64fb6a96f55332096068
Lucas Alves <Ei.lucas.alves@outlook.com>
2023-01-15 08:45:29 -0300

    Aula032_3

---
+++ Python_Básico/Aula032_3.py
@@ -0,0 +1,6 @@
+"""
+Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou 
+menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva 
+"Seu nome é normal"; maior que 6 escreva "Seu nome é muito grande". 
+"""
+
