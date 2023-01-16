589eef3e13994cb3a1664687c84d4aee473d0e84
Lucas Alves <Ei.lucas.alves@outlook.com>
2023-01-16 08:33:16 -0300

    Aula032_3
    Exercício 3

--- Python_Básico/Aula032_3.py
+++ Python_Básico/Aula032_3.py
@@ -4,3 +4,14 @@ menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva
 "Seu nome é normal"; maior que 6 escreva "Seu nome é muito grande". 
 """
 
+nome = input('Digite seu primeiro nome: ')
+tamanho_nome = len(nome)
+
+if tamanho_nome <= 4:
+    print('Seu nome é curto')
+elif tamanho_nome >= 5 and tamanho_nome <=6:
+    print('Seu nome é normal')   
+elif tamanho_nome > 6:
+    print('Seu nome é muito grande')  
+else:
+    print('Digite alguma coisa.')
