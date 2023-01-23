4eef526c7da10ab1fc83b2d7bff389e16648c922
Lucas Alves <Ei.lucas.alves@outlook.com>
2023-01-23 18:44:07 -0300

    Aula035
    Operadores de atribuição

---
+++ Python_Básico/Aula036.py
@@ -0,0 +1,19 @@
+# Operadores de atribuição
+# = += -= /= //= **= %=
+
+contador = 5
+
+contador += 5
+print(contador)
+
+contador -= 5
+print(contador)
+
+contador *= 5
+print(contador)
+
+contador /= 5
+print(contador)
+
+
+
