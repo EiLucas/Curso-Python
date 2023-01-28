8589b0911097ee396589ad1e8a21e83c37abee61
Lucas Alves <Ei.lucas.alves@outlook.com>
2023-01-28 10:01:50 -0300

    Aula039
    Add*

--- Python_Básico/Aula039.py
+++ Python_Básico/Aula039.py
@@ -5,7 +5,8 @@ novo_nome = ''
 indice = 0
 while indice < len(nome):
     letra = nome[indice]
-    novo_nome += letra
+    novo_nome += f'*{letra}'
     indice += 1
 
+novo_nome += '*'
 print(novo_nome)
